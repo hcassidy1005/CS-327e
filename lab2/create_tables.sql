bb080f4be21bcb44371a2b7caf33f687eaebc148
Hailey Cassidy <HaileyCassidy@wireless-10-145-24-122.public.utexas.edu>
2018-02-02 12:49:07 -0600

    Adding lab 2

---
+++ lab2/create_tables.sql
@@ -0,0 +1,23 @@
+drop table if exists Calendar;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_calendar.sql
+
+drop table if exists Listings;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_listings.sql
+
+drop table if exists Neighbourhoods;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_neighbourhoods.sql
+
+drop table if exists Reviews;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_reviews.sql
+
+drop table if exists Summary_Listings;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_summary_listings.sql
+
+drop table if exists Summary_Reviews;
+
+\i /Users/HaileyCassidy/Documents/UT/Spring_2018/Databases/create_summary_reviews.sql
---
+++ lab2/import_data.sql
@@ -0,0 +1,6 @@
+\copy Calendar from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/calendar.csv' (format csv, header true);
+\copy Listings from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/listings.csv' (format csv, header true);
+\copy Neighbourhoods from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/neighbourhoods.csv' (format csv, header true);
+\copy Reviews from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/reviews.csv' (format csv, header true);
+\copy Summary_Reviews from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/summary_reviews.csv' (format csv, header true);
+\copy Summary_Listings from '/User/HaileyCassidy/Documents/UT/Spring_2018/Databases/2017-03-07/summary_listings.csv' (format csv, header true);
